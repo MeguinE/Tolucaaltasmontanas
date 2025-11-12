f2292ea1b54890232b45d964fbd8ef5433259c54
Guillermo <guillermo.diaz.r.engineer@gmail.com>
2025-11-12 13:43:24 -0600

    Se caragaron las demas secciones y corrigio diseño

---
+++ src/data/galery.js
@@ -0,0 +1,16 @@
+// src/data/galery.js
+export const galleryImages = [
+  {
+    src: "src/assets/img/descarga (1).jpeg",
+    alt: "Foto 1",
+  },
+  {
+    src: "src/assets/img/descarga.jpeg",
+    alt: "Foto 2",
+  },
+  {
+    src: "src/assets/img/training.jpg",
+    alt: "Foto 3",
+  },
+  // ...más imágenes
+];
