ff56c4d9fde3704a526611d06c5796e32e2aed95
Guillermo <guillermo.diaz.r.engineer@gmail.com>
2025-11-11 23:10:04 -0600

    se creo la section de entrenamientos y se cragaron los datso de cada categoria

---
+++ src/data/TrainingData.js
@@ -0,0 +1,107 @@
+export const trainingData = [
+  {
+    id: 1,
+    category: "Dientes de Leche",
+    years: "2020 / 2019",
+    image: "src/assets/img/descarga (1).jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 2,
+    category: "Chupón",
+    years: "2018 / 2017",
+    image: "src/assets/img/descarga (1).jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 3,
+    category: "Biberón",
+    years: "2016 / 2015",
+    image: "src/assets/img/training.jpg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 4,
+    category: "Pony",
+    years: "2014 / 2013",
+    image: "src/assets/img/descarga.jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 5,
+    category: "Infantil",
+    years: "2012 / 2011",
+    image: "src/assets/img/descarga (1).jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 6,
+    category: "Intermedia",
+    years: "2010 / 2009",
+    image: "src/assets/img/descarga (1).jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+  {
+    id: 7,
+    category: "Juvenil",
+    years: "2008 / 2007",
+    image: "src/assets/img/descarga (1).jpeg",
+    schedule: [
+      { day: "Lunes", time: "4:00 PM - 6:00 PM" },
+      { day: "Miércoles", time: "4:00 PM - 6:00 PM" },
+      { note: "Tercer día de entrenamiento dependiendo de la categoría." },
+    ],
+    locations: [
+      { name: "Estadio 7 de Enero", address: "Río Blanco, Veracruz" },
+      { name: "Campo Jalapilla", address: "Jalapilla, Rafael Delgado" },
+    ],
+  },
+];
